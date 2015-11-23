5339206e3c850b76b74c6d130c3699eaeff6b55b
Daniel Moennich <daniel.moennich@gmail.com>
2015-11-23 13:24:48 +0100

    added tests for sheet directive

--- test/browser/modules/common/directives/sheet/sheetSpec.js
+++ test/browser/modules/common/directives/sheet/sheetSpec.js
@@ -1,12 +1,12 @@
 "use strict";
 
-describe("Sheet directive", () => {
+describe("The Sheet directive", () => {
 
-	let $compile, $rootScope;
+	let $compile, $rootScope, sheetElement;
 
 	beforeEach(module("sheetsApp"));
 
-	// puts directives HTML template in $templateCache (ng-html2js preprocessor)
+	// puts directives HTML template in $templateCache (provided by ng-html2js preprocessor)
 	beforeEach(module("/modules/common/directives/sheet/sheet.html"));
 
 	beforeEach(inject((_$compile_, _$rootScope_) => {
@@ -15,17 +15,35 @@ describe("Sheet directive", () => {
 	}));
 
 
-	it("adresses the modal with correct ID", () => {
+	beforeEach(() => {
 		$rootScope.testSheet = {
 			id: 5411,
 			title: "some todo list",
 			content: "buy milk"
 		};
-
-		let element = $compile("<sheet data='testSheet'></sheet>")($rootScope);
+		sheetElement = $compile("<sheet data='testSheet'></sheet>")($rootScope);
 		$rootScope.$digest();
-		expect(element.find("img").attr("data-target")).toEqual("#sheet_modal_" + $rootScope.testSheet.id);
-		expect(element.find("div").attr("id")).toEqual("sheet_modal_" + $rootScope.testSheet.id);
 	});
+
+
+	it("adresses the modal with correct ID", () => {
+		expect(sheetElement.find(".img-thumbnail").attr("data-target")).toEqual("#sheet_modal_" + $rootScope.testSheet.id);
+		expect(sheetElement.find(".modal").attr("id")).toEqual("sheet_modal_" + $rootScope.testSheet.id);
+	});
+
+
+	describe("modal", () => {
+
+		it("contains the sheet title", () => {
+			expect(sheetElement.find(".modal-title").text()).toContain($rootScope.testSheet.title);
+		});
+
+		it("contains the sheet content", () => {
+			expect(sheetElement.find(".modal-body").text()).toContain($rootScope.testSheet.content);
+		});
+
+	});
+
+
 
 });
